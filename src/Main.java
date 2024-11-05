6eb1821057bb8f2cd3fed0e215991bc5fceb5041
milaneuh <milanrgmnt@gmail.com>
2024-11-05 23:36:57 +0100

    Fix standard charset

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -37,7 +38,7 @@ class Gauntlet {
 
     private static void runFile(String arg) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(arg));
-        run(new String(bytes, Charset.forName("UTF-8")));  // Explicit UTF-8 encoding
+        run(new String(bytes, StandardCharsets.UTF_8));  // Explicit UTF-8 encoding
 
         if (hasError) System.exit(65);
     }
