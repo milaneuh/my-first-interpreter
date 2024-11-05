2b761f4a5e85eb5d2e57c1a886559b44fc9386de
milaneuh <milanrgmnt@gmail.com>
2024-11-05 17:31:03 +0100

    Clean some codes

--- src/Main.java
+++ src/Main.java
@@ -9,26 +9,24 @@ import java.util.List;
 class Gauntlet {
     private static boolean hasError = false;
 
-    public static void main(String[] args) {
-        if(args.length > 1){
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
             System.out.println("Usage : gauntlet [script]");
             System.exit(64);
-        }else {
-            try {
-                if (args.length == 1){
-                    runFile(args[0]);
-                }
-                else runPrompt();
-            }catch (IOException e){
+        } else {
+            if (args.length == 1) {
+                runFile(args[0]);
+            } else runPrompt();
         }
-        if (hasError) System.exit(65);            }
+        if (hasError) System.exit(65);
     }
 
-    private static void runPrompt() throws IOException  {
+
+    private static void runPrompt() throws IOException {
         InputStreamReader inputStreamReader = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(inputStreamReader);
 
-        for (;;){
+        for (; ; ) {
             System.out.println("> ");
             String line = reader.readLine();
             assert line != null : "The input should not be null.";
@@ -45,40 +43,33 @@ class Gauntlet {
         Scanner scanner = new Scanner(src);
         List<Token> tokens = scanner.scanTokens();
 
-        tokens.forEach(token -> System.out.println(token));
-
+        assert tokens != null : "Tokens should not be null.";
+        tokens.forEach(System.out::println);
     }
 
     //Error Handler
-    static void error(int line, String message){
-        report(line,"",message);
+    static void error(int line, String message) {
+        report(line, "", message);
     }
 
     private static void report(int line, String context, String message) {
-        System.err.println(
-                "Error on line : "+line+"; "+context+" : "+message
-        );
-        hasError =true;
+        System.err.println("Error on line : " + line + "; " + context + " : " + message);
+        hasError = true;
     }
 }
 
 enum TokenType {
     // Single-character tokens.
-    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
 
     // One or two character tokens.
-    BANG, BANG_EQUAL,
-    EQUAL, EQUAL_EQUAL,
-    GREATER, GREATER_EQUAL,
-    LESS, LESS_EQUAL,
+    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
 
     // Literals.
     IDENTIFIER, STRING, NUMBER,
 
     // Keywords.
-    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
-    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR, PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
 
     EOF
 }
@@ -89,14 +80,8 @@ record Token(TokenType type, String lexeme, Object litteral, int line) {
     }
 }
 
-class Scanner {
-    final String source;
-
-    Scanner(String source) {
-        this.source = source;
-    }
-
-    public List<Token> scanTokens(){
+record Scanner(String source) {
+    public List<Token> scanTokens() {
         return null;
     }
 }
