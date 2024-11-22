a18af29bad21bac967200a208d1de2af56996e6c
milaneuh <milanrgmnt@gmail.com>
2024-11-22 17:05:04 +0100

    Evaluating more expr

--- src/gauntlet/Interpreter.java
+++ src/gauntlet/Interpreter.java
@@ -1,13 +1,33 @@
 package gauntlet;
 
+import javax.management.RuntimeErrorException;
+
 import static gauntlet.TokenType.MINUS;
 
-public class Interpreter implements Expr.Visitor<Object> {
+public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Object> {
     @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
         Object left = evaluate(expr.left);
         Object right = evaluate(expr.right);
             switch (expr.operator.type()){
+                case BANG_EQUAL -> {
+                    return !isEqual(left,right);
+                }
+                case EQUAL_EQUAL -> {
+                    return isEqual(left,right);
+                }
+                case GREATER -> {
+                    return (double)left > (double) right;
+                }
+                case GREATER_EQUAL -> {
+                    return (double) left >+ (double) right;
+                }
+                case LESS -> {
+                    return (double) left < (double)  right;
+                }
+                case LESS_EQUAL -> {
+                    return (double) left <= (double) right;
+                }
                 case MINUS -> {
                     return (double) left - (double) right;
                 }
@@ -17,10 +37,25 @@ public class Interpreter implements Expr.Visitor<Object> {
                 case STAR -> {
                     return (double) left* (double) right;
                 }
+                case PLUS -> {
+                    if(left instanceof Double && right instanceof Double){
+                        return (double)left + (double) right;
+                    }else{
+                        if (left instanceof String && right instanceof String){
+                            return  (String)left + (String) right;
+                        }
+                    }
+                }
             }
 
             //Unreachable
             return null;
+    }
+
+    private boolean isEqual(Object left, Object right) {
+        if(left == null && right == null  ) return true;
+        if(left == null) return false;
+        return left.equals(right);
     }
 
     @Override
@@ -58,4 +93,42 @@ public class Interpreter implements Expr.Visitor<Object> {
     private Object evaluate(Expr expr){
         return expr.accept(this);
     }
+
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+        evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt) {
+        Object value = evaluate(stmt.expression);
+        System.out.println(stringify(value));
+        return null;
+    }
+
+    void interpret(Expr expression) {
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeErrorException error) {
+            Gauntlet.runtimeError(error);
+        }
+    }
+
+    private String stringify(Object object) {
+        if (object == null) return "nil";
+
+        if (object instanceof Double) {
+            String text = object.toString();
+            if (text.endsWith(".0")) {
+                text = text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+
+        return object.toString();
+    }
+
+
 }
